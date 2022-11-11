9f2b12406f80f457ea9cd9cdd8e1743ffd524d3d
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-11 15:15:02 +0200

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+btnStop.disabled = true;
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', onClickBtnStart);
+btnStop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickBtnStop() {
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+  clearInterval(timerId);
+}
