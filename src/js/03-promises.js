a27a2bce717bc437e44728a9e1c8c261a2e391de
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-14 16:51:25 +0200

    Done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+const onSuccess = ({ position, delay }) => {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+const onError = ({ position, delay }) => {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  let step = Number(event.currentTarget.step.value);
+  let delay = Number(event.currentTarget.delay.value);
+  let amount = Number(event.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, (delay += step))
+      .then(onSuccess)
+      .catch(onError);
   }
 }
